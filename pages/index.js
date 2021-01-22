356b8491a172373cebcd101096d4bc2d5fd61fa5
최덕경 <hjcdg197@gmail.com>
2021-01-22 12:43:42 +0900

    [Lesson 1] Create a Next.js App

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -7,27 +7,22 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
-
           <a href="https://nextjs.org/learn" className="card">
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
-
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className="card"
@@ -35,7 +30,6 @@ export default function Home() {
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
-
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="card"
@@ -47,7 +41,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-
       <footer>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -58,33 +51,32 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
-
       <style jsx>{`
         .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          min-height: 100vh;
+          padding: 0 0.5rem;
         }
 
         main {
-          padding: 5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          padding: 5rem 0;
         }
 
         footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
           display: flex;
           justify-content: center;
           align-items: center;
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
         }
 
         footer img {
@@ -98,13 +90,13 @@ export default function Home() {
         }
 
         a {
-          color: inherit;
           text-decoration: none;
+          color: inherit;
         }
 
         .title a {
-          color: #0070f3;
           text-decoration: none;
+          color: #0070f3;
         }
 
         .title a:hover,
@@ -115,8 +107,8 @@ export default function Home() {
 
         .title {
           margin: 0;
-          line-height: 1.15;
           font-size: 4rem;
+          line-height: 1.15;
         }
 
         .title,
@@ -125,46 +117,45 @@ export default function Home() {
         }
 
         .description {
-          line-height: 1.5;
           font-size: 1.5rem;
+          line-height: 1.5;
         }
 
         code {
-          background: #fafafa;
-          border-radius: 5px;
           padding: 0.75rem;
-          font-size: 1.1rem;
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          font-size: 1.1rem;
+          border-radius: 5px;
+          background: #fafafa;
         }
 
         .grid {
+          max-width: 800px;
+          margin-top: 3rem;
           display: flex;
           align-items: center;
           justify-content: center;
           flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
         }
 
         .card {
-          margin: 1rem;
           flex-basis: 45%;
           padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
+          margin: 1rem;
           border: 1px solid #eaeaea;
           border-radius: 10px;
+          text-decoration: none;
+          color: inherit;
+          text-align: left;
           transition: color 0.15s ease, border-color 0.15s ease;
         }
 
         .card:hover,
         .card:focus,
         .card:active {
-          color: #0070f3;
           border-color: #0070f3;
+          color: #0070f3;
         }
 
         .card h3 {
@@ -184,12 +175,11 @@ export default function Home() {
 
         @media (max-width: 600px) {
           .grid {
-            width: 100%;
             flex-direction: column;
+            width: 100%;
           }
         }
       `}</style>
-
       <style jsx global>{`
         html,
         body {
@@ -205,5 +195,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
